06089180079b006b2ee2c8a55244246c172084c1
8deniskim8 <raidendemon77@gmail.com>
2018-03-16 16:39:38 -0700

    disabled button

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,47 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      todoInput: '',
-      todoList: []
-    }
-  }
-
-    addTodo(){
-
-      const todoList = this.state.todoList;
-      todoList.push(this.state.todoInput);
-
-      this.setState({
-        todoList,
-          todoInput: '',
-      });
+        this.state = {
+            todoInput: '',
+            todoList: []
+        }
     }
 
-  render() {
-    return (
-      <div>
+    addTodo() {
 
-      <input
-          type="text"
-          value={this.state.todoInput}
-          onChange={(e) => this.setState({ todoInput: e.target.value })}
-      />
-      <button onClick={() => this.addTodo()}>Add</button>
+        const todoList = this.state.todoList;
+        todoList.push(this.state.todoInput);
 
-        <ul>
+        this.setState({
+            todoList,
+            todoInput: '',
+            todoInputValid: false,
+        });
+    }
 
-            {this.state.todoList.map(el => <li key={el}>{el}</li>)}
+    inputChange(v) {
+        let todoInputValid = v.length >= 3;
+        this.setState({
+            todoInput: v,
+            todoInputValid
+        });
+    }
 
-        </ul>
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container pt-4">
+                <div className="row">
+                    <div className="col border">
+                        <ul>
+                            {this.state.todoList.map(el => <li key={el}>{el}</li>)}
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <div className="input-group">
+                            <input
+                                className="form-control"
+                                type="text"
+                                value={this.state.todoInput}
+                                onChange={(e) => this.inputChange(e.target.value)}
+                            />
+                            <div className="input-group-append">
+                                <button
+                                    disabled={!this.state.todoInputValid}
+                                    className="btn btn-outline-primary" onClick={() => this.addTodo()}>Add</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
